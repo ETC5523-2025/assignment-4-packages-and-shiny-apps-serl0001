8f89f895de726ed0d5fed43752b9bb7b2186c564
Sandhya <sandhya@CHANDRASEKARs-MacBook-Pro.local>
2025-11-05 00:00:20 +1100

    minor changes

--- haiexplorer/R/launch_app.R
+++ haiexplorer/R/launch_app.R
@@ -1,8 +1,6 @@
 #' Launch the HAI Explorer app
 #' @return Invisibly, the shiny app object
-#' @examples
-#' \dontrun{ haiexplorer::launch_app() }
-#' @export
+
 launch_app <- function() {
   appdir <- system.file("app", package = utils::packageName())
   shiny::shinyAppFile(file.path(appdir, "app.R"))
